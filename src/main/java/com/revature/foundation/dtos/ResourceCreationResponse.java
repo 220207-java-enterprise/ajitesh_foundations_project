3fd8265ebd26371483799ad4f58db836a904d145
Ajitesh Vedula <Ajitesh@Ajiteshs-MBP.hsd1.ca.comcast.net>
2022-02-25 12:12:57 -0800

    New User class added

---
+++ src/main/java/com/revature/foundation/dtos/ResourceCreationResponse.java
@@ -0,0 +1,21 @@
+package com.revature.foundation.dtos;
+
+
+public class ResourceCreationResponse {
+
+    private String id;
+
+    public ResourceCreationResponse(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
+
