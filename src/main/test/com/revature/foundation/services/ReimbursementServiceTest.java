fd4497861a341e064a684bd3861494d9a663ed75
Ajitesh Vedula <Ajitesh@Ajiteshs-MBP.hsd1.ca.comcast.net>
2022-03-03 09:26:29 -0800

    Reimbursement testing started

--- src/main/test/com/revature/foundation/services/ReimbursementServiceTest.java
+++ src/main/test/com/revature/foundation/services/ReimbursementServiceTest.java
@@ -1,6 +1,37 @@
 package com.revature.foundation.services;
 
+import com.revature.foundation.daos.ReimbursementDAO;
+import com.revature.foundation.daos.UserDAO;
+import com.revature.foundation.dtos.requests.ReimbursementRequest;
+import com.revature.foundation.models.Reimbursement;
+import com.revature.foundation.models.ReimbursementStatus;
+import com.revature.foundation.models.ReimbursementType;
+import com.revature.foundation.util.exceptions.AuthenticationException;
+import com.revature.foundation.util.exceptions.DataSourceException;
+import com.revature.foundation.util.exceptions.InvalidRequestException;
+import org.junit.*;
+import org.mockito.Mockito;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class ReimbursementServiceTest {
+
+    private ReimbursementService sut;
+    private ReimbursementDAO mockReimbursementDao = mock(ReimbursementDAO.class);
+
+    @Before
+    public void setup() {
+        sut = new ReimbursementService(mockReimbursementDao);
+    }
+
+    @After
+    public void cleanUp() {
+        reset(mockReimbursementDao);
+    }
+
+
 
 }
